b217be2c5d9b291c2a7e06007db288c0832291a0
Dmitrii Grechko <prime.optimys@mail.ru>
2025-03-22 09:54:36 +0500

    Module views.py: rename function main to web_site_main edit function web_site_main

--- src/views.py
+++ src/views.py
@@ -11,15 +11,25 @@ from src.utils import (
     get_top_operations,
     get_unique_card_number,
     read_xlsx,
-    selecting_data_by_date,
+    selecting_data_by_date, convert_stock_price,
 )
 
 
-def main() -> None:
+def web_site_main() -> None:
     """
     Функция запуска страницы "Главная"
     """
-    data = selecting_data_by_date(read_xlsx(PATH_TO_OPERATIONS), checking_date_from_user("2021-08-12 12:03:55"))
+    user_input = ""
+    while True:
+        try:
+            user_input = checking_date_from_user(input("Введите дату для анализа в формате YYYY-MM-DD HH:MM:SS:\n"))
+        except ValueError:
+            print("Формат ввода не соответствует требованиям!\n")
+            continue
+        else:
+            break
+
+    data = selecting_data_by_date(read_xlsx(PATH_TO_OPERATIONS), user_input)
 
     cards_info = []
     for card in get_unique_card_number(data):
@@ -29,16 +39,18 @@ def main() -> None:
         card_info = {"last_digits": card[-4:], "total_spent": total_expenses, "cashback": cashback}
         cards_info.append(card_info)
 
+    stock = api_currency_stocks()
+
     response = {
         "greeting": get_greeting(),
         "cards": cards_info,
         "top_transactions": get_top_operations(data),
         "currency_rates": api_currency_rates(),
-        "stock_prices": api_currency_stocks(),
+        "stock_prices": convert_stock_price(stock),
     }
     with open("../data/output_data.json", "w", encoding="utf-8") as file_json:
         json.dump(response, file_json, ensure_ascii=False, indent=4)  # type: ignore
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    web_site_main()
