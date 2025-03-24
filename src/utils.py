49a8a08b38b52bdb0ff3ab805598a64dd93de842
Dmitrii Grechko <prime.optimys@mail.ru>
2025-03-24 14:37:44 +0500

    add logger in utils.py and views.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from datetime import datetime
 from pathlib import Path
@@ -7,7 +8,14 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 
-from config import PATH_TO_USER_SETTINGS
+from config import PATH_TO_LOGGER, PATH_TO_OPERATIONS, PATH_TO_USER_SETTINGS
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(PATH_TO_LOGGER, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_xlsx(xlsx_file_path: Path) -> pd.DataFrame:
@@ -16,6 +24,7 @@ def read_xlsx(xlsx_file_path: Path) -> pd.DataFrame:
     и возвращает DataFrame с банковскими операциями.
     """
     transactions_df = pd.read_excel(xlsx_file_path)
+    logger.info(f"{read_xlsx.__name__} Чтение данных из файла {PATH_TO_OPERATIONS}")
 
     return transactions_df
 
@@ -26,6 +35,7 @@ def checking_date_from_user(user_input_date: str) -> str:
     проверяет ее на соответствие шаблону ввода, в случае совпадения возвращает строку в формате YYYY-MM-DD HH:MM:SS.
     """
     datetime.strptime(user_input_date, "%Y-%m-%d %H:%M:%S")
+    logger.info(f"{checking_date_from_user.__name__} Проверка ввода от пользователя")
     return user_input_date
 
 
@@ -47,6 +57,7 @@ def selecting_data_by_date(data_for_selection: pd.DataFrame, user_input_datetime
         & (data_for_selection["Дата операции"] <= user_datetime)
         & (data_for_selection["Статус"] == "OK")
     ]
+    logger.info(f"{selecting_data_by_date.__name__} Фильтрация данных по дате")
     return filtered_data
 
 
@@ -54,6 +65,7 @@ def get_greeting() -> str:
     """
     Функция возвращает строку приветствия в зависимости от текущего времени суток.
     """
+    logger.info(f"{get_greeting.__name__} Выбор приветствия")
     current_time = datetime.now().time()
     hour = current_time.hour
     minute = current_time.minute
@@ -77,19 +89,22 @@ def get_unique_card_number(data_df: pd.DataFrame) -> list:
     Функция получения уникальных номеров карт
     """
     unique_cards = list(data_df["Номер карты"].dropna().unique())
+    logger.info(f"{get_unique_card_number.__name__} Получен список номеров банковских карт")
     return unique_cards
 
 
 def calculate_total_expenses(data_df: pd.DataFrame, card_number: str) -> float:
     card_data = data_df[data_df["Номер карты"] == card_number]
     total_expenses_calc = float(round(abs(card_data[card_data["Сумма платежа"] < 0]["Сумма платежа"].sum()), 2))
+    logger.info(f"{calculate_total_expenses.__name__} Расчет суммы операций по платежам")
     return total_expenses_calc
 
 
 def calculate_cashback(input_total_expenses: float) -> int:
     """
-    Функция для расчета кешбэка (1 рубль на каждые 100 рублей)
+    Функция для расчета кешбэк (1 рубль на каждые 100 рублей)
     """
+    logger.info(f"{calculate_cashback.__name__} Посчитан кешбэк")
     return int(input_total_expenses // 100)
 
 
@@ -97,9 +112,7 @@ def get_top_operations(data_df: pd.DataFrame) -> list[dict]:
     """
     Функция для получения топ-5 транзакций по сумме платежа
     """
-    operations_data = (data_df.nlargest(5, "Сумма платежа")[
-        ["Дата платежа", "Сумма платежа", "Категория", "Описание"]
-    ])
+    operations_data = data_df.nlargest(5, "Сумма платежа")[["Дата платежа", "Сумма платежа", "Категория", "Описание"]]
 
     operations = operations_data.to_dict(orient="records")
     top_operations = []
@@ -111,7 +124,7 @@ def get_top_operations(data_df: pd.DataFrame) -> list[dict]:
             "description": operation["Описание"],
         }
         top_operations.append(operations_rename_key)
-
+    logger.info(f"{get_top_operations.__name__} Получен список топ-5 по сумме операций")
     return top_operations
 
 
@@ -119,8 +132,10 @@ def get_settings_from_file() -> dict:
     """
     Функция получения пользовательских настроек из файла user_settings.json
     """
+
     with open(PATH_TO_USER_SETTINGS, encoding="utf-8") as file_json:
         data_from_file = dict(json.load(file_json))
+        logger.info(f"{get_settings_from_file.__name__} Получены данные пользовательских настроек из файла")
         return data_from_file
 
 
@@ -142,7 +157,7 @@ def api_currency_rates() -> list[dict]:
         currency_rate = json.loads(response.text)
         rates = currency_rate.get("rates")
         result.append({"currency": base, "rate": round(rates["RUB"], 2)})
-
+    logger.info(f"{api_currency_rates.__name__} Получены данные курса валют из внешнего API")
     return result
 
 
@@ -161,7 +176,7 @@ def api_convert_currency() -> float:
     currency_rate = json.loads(response.text)
     rates = currency_rate.get("rates")
     usd_rate = round(rates["RUB"], 2)
-
+    logger.info(f"{api_convert_currency.__name__} Получен курс USD для конвертации стоимости акций")
     return float(usd_rate)
 
 
@@ -185,8 +200,8 @@ def api_currency_stocks() -> list[dict]:
         stocks = json.loads(response.text)
         stock_data = stocks["Meta Data"]["3. Last Refreshed"]
         stock_price = stocks["Time Series (Daily)"][stock_data]["4. close"]
-
         result.append({"stock": stock, "price": stock_price})
+        logger.info(f"{api_currency_stocks.__name__} Получена стоимость акций")
     return result
 
 
@@ -197,4 +212,5 @@ def convert_stock_price(stock_price: list[dict]) -> list[dict]:
     usd_rate = api_convert_currency()
     for value in stock_price:
         value["price"] = round(usd_rate * float(value["price"]), 2)
+        logger.info(f"{convert_stock_price.__name__} Конвертация стоимости акций из USD в RUB")
     return stock_price
